429f99cd555eec1dc2dac664271cd22a9fda530e
OwenBOSS <ohhodges@gmail.com>
2022-03-21 11:33:18 -0700

    Started TicTacToe

---
+++ Projects/TicTacToe/sketch.js
@@ -0,0 +1,18 @@
+//Props
+var fr = 60;
+var canvasHeight = 500;
+var canvasWidth = 500;
+
+//Member Vars
+
+
+
+function setup() {
+  // put setup code here
+    createCanvas(canvasWidth, canvasHeight);
+}
+
+function draw() {
+  // put drawing code here
+  rect(0,0,200,200);
+}
