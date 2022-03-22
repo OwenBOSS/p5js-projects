719b58e0b791206a97c35b48f296daf4594116e9
OwenBOSS <ohhodges@gmail.com>
2022-03-22 14:19:19 -0700

    Updated tictaktoe

--- Projects/TicTacToe/sketch.js
+++ Projects/TicTacToe/sketch.js
@@ -2,17 +2,38 @@
 var fr = 60;
 var canvasHeight = 500;
 var canvasWidth = 500;
+var offset = 100;
 
 //Member Vars
+let grid;
 
 
 
 function setup() {
   // put setup code here
     createCanvas(canvasWidth, canvasHeight);
+    grid = createGrid();
 }
 
 function draw() {
   // put drawing code here
-  rect(0,0,200,200);
+  for(i=0; i<3; i++){
+    for(j=0; j < 3; j++){
+      grid[i][j].show();
+    }
+  }
+}
+
+function createGrid(){
+  var num = 0;
+  let list = new Array(3);
+  for(i=0; i<3; i++){
+    list[i]=new Array(3);
+    for(j=0; j < 3; j++){
+      list[i][j] = new Tile(createVector(offset + i * 50, offset + j* 50), 50, 50);
+      num++;
+    }
+  }
+  console.log(num);
+  return list;
 }
--- Projects/TicTacToe/tile.js
+++ Projects/TicTacToe/tile.js
@@ -1,4 +1,33 @@
 class Tile{
-    constructor(){
+    constructor(position, height, width){
+        this.position = position;
+        this.height = height;
+        this.width = width;
+        this.marked = false;
+        this.player = "NONE";
+    }
+
+    show(){
+        if(this.isOver()){fill(100);}
+        else{fill(255);}
+        stroke(0);
+        rectMode(CENTER);
+        rect(this.position.x, this.position.y, this.width, this.height);
+        if(this.player == "NONE"){ this.player = " ";}
+        text(this.player, this.position.x - 5, this.position.y+ 5);
+    }
+
+    isOver(){
+        var out = false;
+        var x = this.width * 0.5;
+        var y = this.height * 0.5;
+        if(mouseX > this.position.x - x && mouseX < this.position.x + x 
+            && mouseY > this.position.y - y && mouseY < this.position.y + y){ out = true; }
+        else { out = false; }
+        return out;
+    }
+
+    update(player){
+        if(this.isOver()){this.player=player;}}
     }
 }
